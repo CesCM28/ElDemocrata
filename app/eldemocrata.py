c1b06e61e3165209d41b3f2221d85147c3b878e3
CesCM28 <ces27mill@gmail.com>
2022-05-18 11:20:20 -0600

    consulta de la bd, para mostrar categorias

--- app/eldemocrata.py
+++ app/eldemocrata.py
@@ -1,9 +1,17 @@
+from unicodedata import category
 from flask import (
     Blueprint, render_template
 )
+from db import get_db
 
 bp = Blueprint('eldemocrata', __name__, url_prefix='/')
 
 @bp.route('/', methods=['GET'])
 def index():
-    return render_template('eldemocrata/index.html')
+    db, c = get_db()
+    c.execute(
+        'select * from category where status = 1 order by order'
+    )
+    categorys = c.fechall()
+
+    return render_template('eldemocrata/index.html', categorys=categorys)
