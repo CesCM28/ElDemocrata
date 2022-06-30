bb079fe06248d433c7f63ee91e7ef80bd5637746
CesCM28 <ces27mill@gmail.com>
2022-06-30 13:59:28 -0600

    iconos

--- static/script.js
+++ static/script.js
@@ -1,17 +1,17 @@
 window.addEventListener('load', () => {
 
     let temperaturaValor = document.getElementById("temperatura-valor")
-    let temperaturaCiudad = document.getElementById("temperatura-ciudad")
+    //let temperaturaCiudad = document.getElementById("temperatura-ciudad")
+    let temperaturaIcon = document.getElementById("temperatura-icono")
 
     const urlCh = `https://api.openweathermap.org/data/2.5/weather?q=Chihuahua&lang=es&appid=118fce7e8a401d0ba946654ca50a2bd6&units=metric`
         //console.log(url)
         fetch(urlCh)
             .then(response => { return response.json() })
             .then(data => {
-                console.log(data)
-                let temp = Math.round(data.main.temp)
-                temperaturaValor.textContent = `${temp} °C`
-                temperaturaCiudad.textContent = data.name
+                temperaturaValor.textContent = `${data.main.temp} °`
+                //temperaturaCiudad.textContent = data.name
+                temperaturaIcon.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`
             })
             .catch(error => {
                 console.log(error)
@@ -19,19 +19,16 @@ window.addEventListener('load', () => {
 
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(position => {
-            console.log(position);
             lat = position.coords.latitude
             lon = position.coords.longitude
 
             const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&lang=es&appid=118fce7e8a401d0ba946654ca50a2bd6&units=metric`
-            console.log(url)
             fetch(url)
                 .then(response => { return response.json() })
                 .then(data => {
-                    console.log(data.main.temp)
-                    let temp = Math.round(data.main.temp)
-                    temperaturaValor.textContent = `${temp} °C`
-                    temperaturaCiudad.textContent = data.name
+                    temperaturaValor.textContent = `${data.main.temp} °`
+                    //temperaturaCiudad.textContent = data.name
+                    temperaturaIcon.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`
                 })
                 .catch(error => {
                     console.log(error)
